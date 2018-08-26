d2f4193761fe504b04784a5f2d2714e71802cf39
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-08-26 21:12:26 +0700

    update env

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,7 +5,6 @@ import { readFileSync } from "fs";
 import { join } from "path";
 import * as prettyError from "pretty-error";
 
-import setup from "./setup";
 import resolvers from "./resolvers";
 import context from "./context";
 
@@ -13,7 +12,7 @@ if (process.env.NODE_ENV !== "production") {
   prettyError.start();
 }
 
-createConnection(setup).then((connection) => {
+createConnection().then((connection) => {
   console.log("[database] connected!");
   const typeDefs = readFileSync(join(__dirname, "./schema.graphql"), "utf8");
   const server = new GraphQLServer({ resolvers, typeDefs, context });
